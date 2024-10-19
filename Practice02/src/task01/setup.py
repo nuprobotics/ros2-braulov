94cea51d6ffd94e62d6534a652bc0912474cb370
braulov <mr.braulov@mail.ru>
2024-10-19 23:28:22 +0300

    task01.3

--- Practice02/src/task01/setup.py
+++ Practice02/src/task01/setup.py
@@ -5,7 +5,7 @@ package_name = 'task01'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -16,8 +16,8 @@ setup(
     zip_safe=True,
     maintainer='root',
     maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Package description',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
